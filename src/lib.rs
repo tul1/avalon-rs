6f7dbea314d35ad1a905c00e9abf3662d138ca47
patricio tula <tula.patricio@gmail.com>
2020-03-28 23:32:25 +0100

    Hello I am mordred

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,95 @@
 pub trait Character {
-    fn who_am_i(&self) -> String;
+    fn who_am_i(&self) -> &EvilCharacterName;
     fn am_i_evil(&self) -> bool;
+    fn am_i_good(&self) -> bool;
+    fn am_i_alive(&self) -> bool;
+    fn am_i_dead(&self) -> bool;
+    fn die(&mut self);
+    fn i_vote_for_mission_to(&mut self, vote: Vote);
+    fn did_i_vote_to_go(&self) -> bool;
+    fn did_i_vote_to_stay(&self) -> bool;
+    fn in_quest_i_will(&mut self, quest: Quest);
+}
+
+#[derive(PartialEq, Debug)]
+pub enum Quest {
+    Fail,
+    Success,
+}
+
+#[derive(PartialEq, Debug)]
+pub enum Vote {
+    Approve,
+    Reject,
+}
+
+#[derive(PartialEq, Debug)]
+pub enum EvilCharacterName {
+    Mordred,
+    MinionOfMordred1,
+    MinionOfMordred2,
+    MinionOfMordred3,
+    Assasin,
+    Oberon,
+    Morgana,
 }
 
 pub struct EvilCharacter {
-    name: String,
-    good: bool,
+    name: EvilCharacterName,
+    alive: bool,
+    vote: Option<Vote>,
+    quest: Option<Quest>,
 }
 
 impl EvilCharacter {
-    pub fn new(name: String) -> Self {
-        EvilCharacter { name, good: false }
+    pub fn new(name: EvilCharacterName) -> Self {
+        EvilCharacter {
+            name,
+            alive: true,
+            vote: None,
+            quest: None,
+        }
     }
 }
 
 impl Character for EvilCharacter {
-    fn who_am_i(&self) -> String {
-        self.name.clone()
+    fn who_am_i(&self) -> &EvilCharacterName {
+        &self.name
     }
+
     fn am_i_evil(&self) -> bool {
-        self.good
+        true
+    }
+
+    fn am_i_good(&self) -> bool {
+        false
+    }
+
+    fn am_i_alive(&self) -> bool {
+        self.alive
+    }
+
+    fn am_i_dead(&self) -> bool {
+        !self.alive
+    }
+
+    fn die(&mut self) {
+        self.alive = false;
+    }
+
+    fn i_vote_for_mission_to(&mut self, vote: Vote) {
+        self.vote = Some(vote);
+    }
+
+    fn did_i_vote_to_go(&self) -> bool {
+        self.vote == Some(Vote::Approve)
+    }
+
+    fn did_i_vote_to_stay(&self) -> bool {
+        self.vote == Some(Vote::Reject)
+    }
+
+    fn in_quest_i_will(&mut self, quest: Quest) {
+        self.quest = Some(quest);
     }
 }
--- tests/EvilCharacter.rs
+++ tests/EvilCharacter.rs
@@ -1,7 +1,77 @@
 use avalon_rs::*;
 
+fn hello_i_am_mordred() -> EvilCharacter {
+    EvilCharacter::new(EvilCharacterName::Mordred)
+}
+
 #[test]
-fn i_am_groot() {
-    let groot = EvilCharacter::new(String::from("pepe"));
-    assert_eq!(groot.who_am_i(), String::from("pepe"));
+fn i_am_mordred() {
+    assert_eq!(*hello_i_am_mordred().who_am_i(), EvilCharacterName::Mordred);
+}
+
+#[test]
+fn i_am_not_mordred() {
+    assert_ne!(*hello_i_am_mordred().who_am_i(), EvilCharacterName::Morgana);
+}
+
+#[test]
+fn is_mordred_evil() {
+    assert_eq!(hello_i_am_mordred().am_i_evil(), true);
+}
+
+#[test]
+fn is_mordred_good() {
+    assert_eq!(hello_i_am_mordred().am_i_good(), false);
+}
+
+#[test]
+fn has_mordred_borned_alive() {
+    assert_eq!(hello_i_am_mordred().am_i_alive(), true);
+}
+
+#[test]
+fn has_mordred_borned_dead() {
+    assert_eq!(hello_i_am_mordred().am_i_dead(), false);
+}
+
+#[test]
+fn is_mordred_really_dead() {
+    let mut player = hello_i_am_mordred();
+    player.die();
+    assert_eq!(player.am_i_dead(), true);
+}
+
+#[test]
+fn is_mordred_a_zombie() {
+    let mut player = hello_i_am_mordred();
+    player.die();
+    assert_eq!(player.am_i_alive(), false);
+}
+
+#[test]
+fn is_mordred_against_when_he_rejects() {
+    let mut player = hello_i_am_mordred();
+    player.i_vote_for_mission_to(Vote::Reject);
+    assert_eq!(player.did_i_vote_to_stay(), true);
+}
+
+#[test]
+fn is_mordred_for_when_he_rejects() {
+    let mut player = hello_i_am_mordred();
+    player.i_vote_for_mission_to(Vote::Reject);
+    assert_eq!(player.did_i_vote_to_go(), false);
+}
+
+#[test]
+fn is_mordred_against_when_he_approves() {
+    let mut player = hello_i_am_mordred();
+    player.i_vote_for_mission_to(Vote::Approve);
+    assert_eq!(player.did_i_vote_to_stay(), false);
+}
+
+#[test]
+fn is_mordred_for_when_he_approves() {
+    let mut player = hello_i_am_mordred();
+    player.i_vote_for_mission_to(Vote::Approve);
+    assert_eq!(player.did_i_vote_to_go(), true);
 }
